164493ba4e5b209d3a8d8258b2f768d385a2ab17
andrii.p.marchenko <marchenko1993.a@gmail.com>
2018-02-21 18:34:41 +0200

    Loose game after the fight

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -13,7 +13,9 @@
    <div>
       <router-link to="/teststore" class="button button-huge block-mobile">TEST STORE</router-link>
    </div>
-
+    <div>
+      <router-link to="/developers" class="button button-huge block-mobile">Developers</router-link>
+    </div>
   </div>
 </template>
 
